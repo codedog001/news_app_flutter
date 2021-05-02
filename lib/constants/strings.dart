dff6ca14ade80e97376bbd474363638d60c8fe4e
Mohit Jain <mohitjain9875@gmail.com>
2021-05-03 01:51:01 +0530

    Google India News

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,4 +1,4 @@
 class Strings {
   static String newsUrl =
-      'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7f1e2f2ea2984ebf90ad742ec07f012a';
+      'https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=7f1e2f2ea2984ebf90ad742ec07f012a';
 }
