f1d462180d7ea06f40bf0428dc797f891c5c1ab8
Mohit Jain <mohitjain9875@gmail.com>
2021-05-02 20:12:34 +0530

    call API

---
+++ lib/constants/strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static String newsUrl =
+      'https://newsapi.org/v2/everything?q=tesla&from=2021-04-02&sortBy=publishedAt&apiKey=7f1e2f2ea2984ebf90ad742ec07f012a';
+}
---
+++ lib/services/api_manager.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pp_news_app_api/models/newsinfo.dart';
+import '../constants/strings.dart';
+
+class API_manager {
+  Future<NewsModels> getNews() async {
+    var client = http.Client();
+    var newsModel = null;
+
+    try {
+      var response = await client.get(Uri.parse(Strings.newsUrl));
+
+      if (response.statusCode == 200) {
+        //Call API get JSON
+        var jsonString = response.body;
+
+        //Map string to JSON object
+        var jsonMap = json.decode(jsonString);
+
+        //Use factory method to get newsModel
+        var newsModel = NewsModels.fromJson(jsonMap);
+      }
+    } catch (e) {
+      return newsModel;
+    }
+  }
+}
